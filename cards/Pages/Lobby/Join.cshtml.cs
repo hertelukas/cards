346b8d55900291c199c97e13ad311b937f35eee0
lukas <luki.hertel@gmail.com>
2022-04-12 18:31:17 +0200

    Created the join menu

--- cards/Pages/Lobby/Join.cshtml.cs
+++ cards/Pages/Lobby/Join.cshtml.cs
@@ -1,4 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+using cards.Data;
+using cards.Data.Game.Decks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace cards.Pages.Lobby;
@@ -6,8 +10,49 @@ namespace cards.Pages.Lobby;
 [Authorize]
 public class JoinModel : PageModel
 {
-    public void OnGet()
+    private readonly ILobbyService _lobbyService;
+
+    public JoinModel(ILobbyService lobbyService)
     {
-        
+        _lobbyService = lobbyService;
+    }
+
+    [BindProperty] public InputModel Input { get; set; }
+
+    public int Id { get; set; }
+
+    public void OnGet(int id)
+    {
+        Id = id;
+    }
+
+    public IActionResult OnPost()
+    {
+        try
+        {
+            var username = User.Identity?.Name;
+            return _lobbyService.JoinLobby(Input.Id, Input.Password, username ?? throw new NullReferenceException())
+                switch
+                {
+                    Data.Response.Success => RedirectToPage("Index", Input.Id),
+                    _ => Page()
+                };
+        }
+        catch (NullReferenceException)
+        {
+            return Page();
+        }
+    }
+
+    public class InputModel
+    {
+        [Required]
+        [Display(Name = "Lobby ID")]
+        public int Id { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password for joining the lobby")]
+        public string Password { get; set; }
     }
 }
