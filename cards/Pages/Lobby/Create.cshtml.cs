db075de0e9f45aa86ce98f21b9eed10f3b77d732
lukas <luki.hertel@gmail.com>
2022-04-12 18:00:02 +0200

    Created the create logic

--- cards/Pages/Lobby/Create.cshtml.cs
+++ cards/Pages/Lobby/Create.cshtml.cs
@@ -1,4 +1,7 @@
+using System.ComponentModel.DataAnnotations;
+using cards.Data;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace cards.Pages.Lobby;
@@ -6,8 +9,50 @@ namespace cards.Pages.Lobby;
 [Authorize]
 public class CreateModel : PageModel
 {
+    private readonly ILobbyService _lobbyService;
+
+    public CreateModel(ILobbyService lobbyService)
+    {
+        _lobbyService = lobbyService;
+    }
+
+    [BindProperty] public InputModel Input { get; set; }
+
     public void OnGet()
     {
-        
+    }
+
+    public IActionResult OnPost()
+    {
+        try
+        {
+            var username = User.Identity?.Name;
+
+            return RedirectToPage("Index",
+                new
+                {
+                    id = _lobbyService.CreateLobby(username ?? throw new NullReferenceException(), Input.Name,
+                        Input.Password)
+                });
+        }
+        catch (NullReferenceException)
+        {
+            return Page();
+        }
+    }
+
+    public class InputModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Lobby Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.",
+            MinimumLength = 6)]
+        [Display(Name = "Password for joining the lobby")]
+        public string Password { get; set; }
     }
 }
