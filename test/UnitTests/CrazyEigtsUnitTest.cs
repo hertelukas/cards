45f23f831fbd994839cd1484ba564ea212922f79
lukas <luki.hertel@gmail.com>
2022-04-17 13:48:03 +0200

    Some simple unit tests for crazy eights

--- test/UnitTests/CrazyEigtsUnitTest.cs
+++ test/UnitTests/CrazyEigtsUnitTest.cs
@@ -1,11 +1,23 @@
+using cards.Data.Game.Implementations;
 using Xunit;
 
 namespace UnitTests;
 
-public class UnitTest1
+public class CrazyEightsUnitTest
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData(2, 7)]
+    [InlineData(3, 5)]
+    [InlineData(7, 5)]
+    public void AmountOfPlayerCards(int players, int cards)
     {
+        var crazyEights = new CrazyEights();
+
+        crazyEights.Initialize(players);
+
+        for (var i = 0; i < players; i++)
+        {
+            Assert.Equal(cards, crazyEights.GetHand(i).Count);
+        }
     }
 }
